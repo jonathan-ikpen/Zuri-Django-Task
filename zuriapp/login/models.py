349807c5d8e687912eba6763a94cdcbaadcb6103
Jonathan Ikpen <jonathanikpen4@gmail.com>
2022-06-03 23:51:43 +0100

    first commit

---
+++ zuriapp/login/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+# class Students(models.Model):
+#     name = models.CharField(max_Length=23)
+#     age = models.CharField(max_length=23)
+
+
+# class Teachers(models.Model):
+#     name = models.CharField(max_length=23)
